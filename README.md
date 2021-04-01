5e5f4d36df7fc64b8f7c102920c7e88fef0332d1
el7777 <leociffer@gmail.com>
2021-04-01 15:19:44 +0700

    FizzBuzz

---
+++ README.md
@@ -0,0 +1,6 @@
+## FizzBuzz
+Given a number n, for each integer i in the range from 1 to n inclusive, print one value per line as follows:
+- if i is a multiple of both 3 and 5, print FizzBuzz
+- if i is a multiple of 3 but not 5, print Fizz
+- if i is a multiple of 5 but not 3, print Buzz
+- if i is not a multiple of 3 or 5 print the value of i
---
+++ fizzbuzz.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func fizzBuzz(n int32) {
+	// Write your code here
+	var i int32
+	for i = 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 && i%5 != 0 {
+			fmt.Println("Fizz")
+		} else if i%3 != 0 && i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	fizzBuzz(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
